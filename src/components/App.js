d8596a9efd35175da46abaaac5d51f1575b33948
Mike el chido <miguel95.hh@outlook.com>
2019-02-20 00:42:12 -0600

    Se creo la vista de las tarjetas

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
-//import logo from './global/images/logo.svg';
-import novios from './global/images/novios.jpg';
+import logo from './global/images/logo.svg';
 import './global/css/App.css';
+import { todos } from './todos.json'
+
+console.log(todos)
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      todos
+    }
+  }
   render() {
+    const todos = this.state.todos.map((todo, i)=>{
+      return(
+        <div className="col-md-3">
+          <div className="card mt-4">
+            <div className="card-header">
+              <h3>{todo.title}</h3>
+              <span className="badge badge-pill badge-danger ml-2">
+                {todo.priority}
+              </span>
+            </div>
+            <div className="card-body">
+              <p>{todo.description}</p>
+              <p><mark>{todo.responsable}</mark></p>
+            </div>
+          </div>
+        </div>
+      )
+    })
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={novios} className="App-logo" alt="logo" />
-          <p>
-            Tengo a la novia más hermosa de todas y la amo con todo mi corazón...
-           </p>
-        </header>
+          <nav className="navbar navbar-dark bg-dark">
+            <a className="text-white" href="./">task
+            <span className="badge bagde-bill badge-light ml-2">
+              {this.state.todos.length}
+            </span></a>
+          </nav>
+          <div className="container">
+            <div className="row mt-3">
+              {todos}
+            </div>
+          </div>
+          <img src={logo} className="App-logo" alt="logo" />
       </div>
     );
   }
